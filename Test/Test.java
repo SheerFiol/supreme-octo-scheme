08d65d1806b280580b76410aa99e96e571d9c181
Sheer <Safiol@uno.edu>
2020-07-05 16:35:54 -0500

    Test

---
+++ Test/Test.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Test{
+	public static void main(String[] args){
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter a number");
+		int num = in.nextInt();
+		num *= 2;
+		System.out.printf("The number is %d after being doubled\n",num);
+	}
+}
